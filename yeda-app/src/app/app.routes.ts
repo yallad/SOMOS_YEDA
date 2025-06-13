2a90eff84d61bdc8f6975f0f9dffb732dd510153
yerika <marleyrozo@gmail.com>
2025-06-12 22:23:58 -0500

    ajustes de ruta

---
+++ yeda-app/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
+];
