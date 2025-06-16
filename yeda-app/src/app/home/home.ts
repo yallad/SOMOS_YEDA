a3e212d4dd493a3d2bb39a595c577b602aad2d05
debbie <debbieaa97@yahoo.com>
2025-06-16 10:35:39 -0500

    actualización home

--- yeda-app/src/app/home/home.ts
+++ yeda-app/src/app/home/home.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './home.css'
 })
 export class Home {
+  currentSlide = 0;
+  slides = [
+    // Aquí puedes poner los datos de tus slides, por ejemplo:
+    { id: 1, content: 'Slide 1' },
+    { id: 2, content: 'Slide 2' },
+    { id: 3, content: 'Slide 3' }
+  ];
 
+  get totalSlides(): number {
+    return this.slides.length;
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.totalSlides;
+  }
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.totalSlides) % this.totalSlides;
+  }
 }
